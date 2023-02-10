d6eca68fdfe050d42e0fb4d1eabc1ba41b643453
bakhtkhilji23 <bakhtkhilji23@gmail.com>
2023-02-10 12:08:33 +0500

    Navigation bar added

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,18 +1,56 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:widgets/widgets/reorderable_listview.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int index = 0;
+  final _bottomNavigationKey = GlobalKey<CurvedNavigationBarState>();
+  @override
   Widget build(BuildContext context) {
+    List<Widget> screens = const [
+      ReorderListWidget(),
+      Center(
+        child: Text("Favourite"),
+      ),
+      Center(
+        child: Text("Setting"),
+      ),
+    ];
+
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text("Reorder List"),
         centerTitle: true,
       ),
-      body: ReorderListWidget(),
+      body: screens[index],
+      bottomNavigationBar: CurvedNavigationBar(
+        buttonBackgroundColor: Colors.amber,
+        color: Colors.purple,
+        backgroundColor: Colors.green,
+        height: 60,
+        index: index,
+        items: const [
+          Icon(
+            Icons.home,
+            size: 30,
+            color: Colors.white,
+          ),
+          Icon(Icons.favorite),
+          Icon(Icons.settings)
+        ],
+        key: _bottomNavigationKey,
+        onTap: (index) => setState(() {
+          this.index = index;
+        }),
+      ),
     );
   }
 }
